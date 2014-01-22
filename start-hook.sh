80365ca5574a6f25fe9028af81c957a3e97a6597
Brian Ford <btford@umich.edu>
2014-01-22 12:01:24 -0800

    ensure scripts run from correct dir

--- start-hook.sh
+++ start-hook.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 #
 # starts a hookshot server that will `git pull` whenever
 # GitHub pings us
--- watch-hook.sh
+++ watch-hook.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 #
 # spawns the hook, and restarts it if it goes down
 #
